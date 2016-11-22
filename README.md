0c6bd2b13298247348c56471e8830c734ee2797a
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2016-11-22 23:11:07 +0200

    v1.0.0
    - Handle old-style log entries as well as newer ones (serialized params,
      log_page)
    - If a page was renamed since being released, it will link to the
      current page but also show the original name as it was upon release.
    - If the WikiRights ArticleType extension is installed, Portals
      will be shown in **bold** and marked as such, to make new portals
      stand out more.

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 WikiRights New Articles extension for MediaWiki
 ===============================================
 
-This MediaWiki extension adds a special page and feed for new articles as defined for the Kol-Zchut website: 
-pages moved from NS_WR_DRAFTS to NS_MAIN.
+This MediaWiki extension adds a special page that shows new articles as
+defined for the Kol-Zchut website: pages moved from NS_WR_DRAFTS to
+NS_MAIN.
 
-## Changelog ##
+## Roadmap
+- Add a feed
 
-### 0.1.0a, 2015-06-23 ###
+## Changelog
+
+### 1.0.0, 2016-11-22
+- Handle old-style log entries as well as newer ones (serialized params,
+  log_page)
+- If a page was renamed since being released, it will link to the
+  current page but also show the original name as it was upon release.
+- If the WikiRights ArticleType extension is installed, Portals 
+  will be shown in **bold** and marked as such, to make new portals
+  stand out more.
+
+### 0.1.0a, 2015-06-23
+First version ever.
--- SpecialNewArticles.php
+++ SpecialNewArticles.php
@@ -18,14 +18,9 @@ class SpecialNewArticles extends SpecialPage {
 	}
 
 	function execute( $query ) {
-		$request = $this->getRequest();
-		$output = $this->getOutput();
-
 		$this->setHeaders();
 
 		$pager = new NewArticlesPager();
-
-		//log_namespace =
 
 		# Insert list
 		$logBody = $pager->getBody();
@@ -39,10 +34,7 @@ class SpecialNewArticles extends SpecialPage {
 			$this->getOutput()->addWikiMsg( 'logempty' );
 		}
 
-
 	}
-
-
 
 }
 
@@ -56,18 +48,38 @@ class NewArticlesPager extends LogPager {
 			null,
 			0
 		);
-		$extraConds = array(
-			'log_namespace = ' . NS_WR_DRAFTS
-		);
+		$extraConds = [
+			'log_namespace' => NS_WR_DRAFTS
+		];
 
 		parent::__construct(
 			$loglist,
-			array( 'move' ),
-			null, null, null,
+			[ 'move' ],
+			'', '', '',
 			$extraConds
 		);
 
+	}
 
+	public function getQueryInfo() {
+		$info = parent::getQueryInfo();
+		$info[ 'fields' ][] = 'log_page';
+
+		// if Extension:WRArticleType is available
+		if ( class_exists( 'WRArticleType' ) ) {
+			$info[ 'tables' ][] = 'page_props';
+			$info[ 'join_conds' ][ 'page_props' ] = [
+				'LEFT JOIN',
+				[
+					'pp_page=log_page',
+					'pp_propname = "ArticleType"'
+				]
+			];
+
+			$info[ 'fields' ][] = 'pp_value';
+		}
+
+		return $info;
 	}
 
 	function formatRow( $row ) {
@@ -78,21 +90,47 @@ class NewArticlesPager extends LogPager {
 			)
 		);
 
-		$params = unserialize( $row->log_params );
-		//print_r( $params ); echo "\n";
-		$newName = $params['4::target'];
-		$pageTitle = Title::newFromText( $newName );
+		$paramsSerialized = unserialize( $row->log_params );
+		$paramsOldFormat = null;
+		// If the params aren't serialized, it's an older log format
+		if ( $paramsSerialized === false ) {
+			$paramsOldFormat = explode( "\n", $row->log_params );
+		}
+
+		$targetName = $paramsSerialized === false ? $paramsOldFormat[0] : $paramsSerialized['4::target'];
+		$logPageId = (int)$row->log_page;
+		$currentTitle = ( $logPageId && $logPageId !== 0 ) ? Title::newFromID( $logPageId ) : null;
+
+		$targetTitleObj = Title::newFromText( $targetName );
 
 		// Make sure the target is NS_MAIN
-		if ( $pageTitle->getNamespace() !== NS_MAIN ) {
+		if ( $targetTitleObj->getNamespace() !== NS_MAIN ) {
 			return false;
 		}
 
-		$pageLink = Linker::linkKnown( $pageTitle );
+		$originalNameDisplay = '';
+		if ( $currentTitle && $targetName !== $currentTitle->getFullText() ) {
+			$originalNameDisplay = ' ' . $this->msg( 'newarticles-original-title' )->params( $targetName );
+		}
+
+		$pageLink = Linker::link( $originalNameDisplay ? $currentTitle : $targetTitleObj );
+		$articleTypeDisplay = '';
+
+		if ( isset( $row->pp_value ) && $row->pp_value === 'portal' ) {
+			$pageLink = Html::rawElement( 'strong', [], $pageLink );
+
+			$articleTypeReadable = WRArticleType::getReadableArticleTypeFromCode( $row->pp_value );
+			$articleTypeDisplay = $this->msg( 'newarticles-articletype' )
+									->params( $articleTypeReadable )->text();
+			$articleTypeDisplay = ' ' . $articleTypeDisplay;
+		}
+
+
+
 		$formattedRow = Html::rawElement(
 			'li',
-			array(),
-			"$time: $pageLink"
+			[],
+			"{$time}: {$pageLink}{$articleTypeDisplay}{$originalNameDisplay}"
 		) . "\n";
 
 		return $formattedRow;
